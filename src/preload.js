27f81e734b6cf53fe68ea40eedb0d105164f9103
chhnaveedulaziz <chhnaveedulaziz@gmail.com>
2023-12-25 19:50:14 +0500

    File changes

---
+++ src/preload.js
@@ -0,0 +1,28 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    myPing() {
+      ipcRenderer.send('ipc-example', 'ping');
+      
+    },
+    myPing2() {
+      
+      ipcRenderer.send('LOGIN', 'pingxxx');
+    },
+    on(channel, func) {
+      const validChannels = ['ipc-example','LOGIN'];
+      if (validChannels.includes(channel)) {
+        // Deliberately strip event as it includes `sender`
+        ipcRenderer.on(channel, (event, ...args) => func(...args));
+      }
+    },
+    once(channel, func) {
+      const validChannels = ['ipc-example','LOGIN'];
+      if (validChannels.includes(channel)) {
+        // Deliberately strip event as it includes `sender`
+        ipcRenderer.once(channel, (event, ...args) => func(...args));
+      }
+    },
+  },
+});
